7e76497b1b94cdad1de5a83409c9ae5b23ea0e90
EugeniaDP <edepalma95@gmail.com>
2023-05-21 15:30:32 -0300

    Edité imágenes del catálogo y seguí con la validación del formulario

--- app.js
+++ app.js
@@ -5,9 +5,37 @@ function validar() {
     var telefono=document.getElementById("telefono");
 
     if (nombre.value==="" || apellido.value==="" || mail.value==="" || telefono.value==="") {
-        alert("Debe completar todos los campos del formulario");
+        alert("Debe completar todos los campos del formulario.");
         return false;
     }
+
+    for (var j = 0; j < telefono.value.length; j++) {
+        var digit = telefono.value.charAt(j);
+        if (digit < "0" || digit > "9") {
+          alert("El campo 'Teléfono' sólo puede contener dígitos numéricos.");
+          return false;
+        }
+    }
+
+    for (var j = 0; j < nombre.value.length; j++) {
+        var char = nombre.value.charAt(j);
+        if (char >= "0" && char <= "9") {
+          alert("El campo 'Nombre' no puede contener dígitos numéricos.");
+          return false;
+        }
+    }
+
+    for (var j = 0; j < apellido.value.length; j++) {
+        var char = apellido.value.charAt(j);
+        if (char >= "0" && char <= "9") {
+          alert("El campo 'Apellido' no puede contener dígitos numéricos.");
+          return false;
+        }
+    }
+
+    alert("Formulario enviado correctamente. Gracias por su consulta.")
+    return true;
+  
 }
 
 
